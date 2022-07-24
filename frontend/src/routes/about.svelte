1540ac8bbc6c41f4cbcd043112e4f198d4aa304b
victorjourne <journe.victor@gmail.com>
2022-07-24 22:27:01 +0200

    :lipstick: Add avatars and cards

--- frontend/src/routes/about.svelte
+++ frontend/src/routes/about.svelte
@@ -2,8 +2,67 @@
   <title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
 <h1>About the team</h1>
-https://gouvfr.atlassian.net/wiki/spaces/DB/pages/222331438/Tuile+-+Tile
+<!-- https://gouvfr.atlassian.net/wiki/spaces/DB/pages/222331438/Tuile+-+Tile-->
+<!-- Tuiles horizontales placées dans la grille -->
+<!-- Tuiles verticales placées dans la grille -->
 
+<div class="fr-grid-row fr-grid-row--gutters">
+    <div class="fr-col-6 fr-col-md-4 fr-col-lg-3">
+        <div class="fr-tile fr-enlarge-link">
+            <div class="fr-tile__body">
+                <h4 class="fr-tile__title">
+                    <a class="fr-tile__link" href>Tristan Solus</a>
+                </h4>
+                <p class="fr-tile__desc">Stagiaire au pôle Léonard de Vinci</p>
+            </div>
+            <div class="fr-tile__img">
+                <img src="avatar/tristan.jpeg" class="fr-responsive-img" alt="">
+                <!-- L'alternative de l'image (attribut alt) doit à priori rester vide car l'image est illustrative et ne doit pas être restituée aux technologies d’assistance. Vous pouvez toutefois remplir l'alternative si vous estimer qu'elle apporte une information essentielle à la compréhension du contenu non présente dans le texte -->
+            </div>
+        </div>
+    </div>
+    <div class="fr-col-6 fr-col-md-4 fr-col-lg-3">
+        <div class="fr-tile fr-enlarge-link">
+            <div class="fr-tile__body">
+                <h4 class="fr-tile__title">
+                    <a class="fr-tile__link" href>Lucas Guerrerero</a>
+                </h4>
+                <p class="fr-tile__desc">Stagiaire Master Informatique à l'Université de Lens</p>
+            </div>
+            <div class="fr-tile__img">
+                <img src="../img/placeholder.1x1.png" class="fr-responsive-img" alt="">
+                <!-- L'alternative de l'image (attribut alt) doit à priori rester vide car l'image est illustrative et ne doit pas être restituée aux technologies d’assistance. Vous pouvez toutefois remplir l'alternative si vous estimer qu'elle apporte une information essentielle à la compréhension du contenu non présente dans le texte -->
+            </div>
+        </div>
+    </div>
+    <div class="fr-col-6 fr-col-md-4 fr-col-lg-3">
+        <div class="fr-tile fr-enlarge-link">
+            <div class="fr-tile__body">
+                <h4 class="fr-tile__title">
+                    <a class="fr-tile__link" href>Pierre-Louis Bouchez</a>
+                </h4>
+                <p class="fr-tile__desc">Stagiaire Master Informatique à l'Université de Lens</p>
+            </div>
+            <div class="fr-tile__img">
+                <img src="avatar/pl.png" class="fr-responsive-img" alt="">
+                <!-- L'alternative de l'image (attribut alt) doit à priori rester vide car l'image est illustrative et ne doit pas être restituée aux technologies d’assistance. Vous pouvez toutefois remplir l'alternative si vous estimer qu'elle apporte une information essentielle à la compréhension du contenu non présente dans le texte -->
+            </div>
+        </div>
+    </div>
+    <div class="fr-col-6 fr-col-md-4 fr-col-lg-3">
+        <div class="fr-tile fr-enlarge-link">
+            <div class="fr-tile__body">
+                <h4 class="fr-tile__title">
+                    <a class="fr-tile__link" href>Victor Journé</a>
+                </h4>
+                <p class="fr-tile__desc">Data Scientist</p>
+            </div>
+            <div class="fr-tile__img">
+                <img src="avatar/victor.jpeg" class="fr-responsive-img" alt="">
+                <!-- L'alternative de l'image (attribut alt) doit à priori rester vide car l'image est illustrative et ne doit pas être restituée aux technologies d’assistance. Vous pouvez toutefois remplir l'alternative si vous estimer qu'elle apporte une information essentielle à la compréhension du contenu non présente dans le texte -->
+            </div>
+        </div>
+    </div>
+</div>
 <p>TODO...</p>
