dd10e5196f6feb43c85e22ffa30d47a16e0d7ad8
victorjourne <journe.victor@gmail.com>
2022-07-25 15:52:22 +0200

    WP : Add missing files

---
+++ frontend/src/routes/link.svelte
@@ -0,0 +1,108 @@
+<svelte:head>
+  <title>Liens utiles</title>
+</svelte:head>
+
+<h2>Les projets proches de TwinCity</h2>
+
+<div class="fr-grid-row fr-grid-row--gutters">
+  <div class="fr-col-6 fr-col-md-4 fr-col-lg-6">
+    <div class="fr-card fr-enlarge-link">
+        <div class="fr-card__body">
+            <div class="fr-card__content">
+                <h4 class="fr-card__title">
+                    <a href="http://carla.org">CARLA</a>
+                </h4>
+                <p class="fr-card__desc">Banc d'essai virtuel pour tester les
+                les pilotes de véhicules autonome. Projet européen open-source
+                développé avec Real Engine.</p>
+                <!-- <div class="fr-card__end">
+                    <p class="fr-card__detail fr-icon-warning-fill">détail (optionnel)</p>
+                </div> -->
+            </div>
+        </div>
+        <div class="fr-card__header">
+            <div class="fr-card__img">
+              <img src="https://carla.readthedocs.io/en/latest/img/objects_small.gif" class="fr-responsive-img" alt="">
+                <!-- L’alternative de l’image (attribut alt) doit toujours être présente, sa valeur peut-être vide (image n’apportant pas de sens supplémentaire au contexte) ou non (porteuse de texte ou apportant du sens) selon votre contexte -->
+            </div>
+        </div>
+      </div>
+  </div>
+  <div class="fr-col-6 fr-col-md-4 fr-col-lg-6">
+      <div class="fr-card fr-enlarge-link">
+          <div class="fr-card__body">
+              <div class="fr-card__content">
+                  <h4 class="fr-card__title">
+                      <a href="http://synthia-dataset.net/">SYNTHIA Dataset</a>
+                  </h4>
+                  <p class="fr-card__desc">L'équipe de Carla, deux ans plus tôt, on
+                  mis à disposition un jeu de données synthétiques pour aider
+                  les algorithmes de traitement d'images pour la voiture autonome.</p>
+                  <!-- <div class="fr-card__end">
+                      <p class="fr-card__detail fr-icon-warning-fill">détail (optionnel)</p>
+                  </div> -->
+              </div>
+          </div>
+          <div class="fr-card__header">
+              <div class="fr-card__img">
+              <img src="http://synthia-dataset.net/wp-content/uploads/2016/06/randcityscapes-e1569359521511.png" class="fr-responsive-img" alt="">
+                  <!-- L’alternative de l’image (attribut alt) doit toujours être présente, sa valeur peut-être vide (image n’apportant pas de sens supplémentaire au contexte) ou non (porteuse de texte ou apportant du sens) selon votre contexte -->
+              </div>
+          </div>
+        </div>
+  </div>
+  <div class="fr-col-6 fr-col-md-4 fr-col-lg-6">
+        <div class="fr-card fr-enlarge-link">
+            <div class="fr-card__body">
+                <div class="fr-card__content">
+                    <h4 class="fr-card__title">
+                        <a href="https://www.spirops.com/">SpirOps</a>
+                    </h4>
+                    <p class="fr-card__desc">Une compagnie privée en intelligence
+                    artificielle, qui mêle le jeu vidéo à ses recherches.</p>
+                    <!-- <div class="fr-card__end">
+                        <p class="fr-card__detail fr-icon-warning-fill">détail (optionnel)</p>
+                    </div> -->
+                </div>
+            </div>
+            <div class="fr-card__header">
+                <div class="fr-card__img">
+                  <video  class="fr-responsive-img" alt="">
+                    <source src="https://www.spirops.com/img/crowd_overview_426_240.mp4">
+                  </video>
+                </div>
+            </div>
+          </div>
+  </div>
+  <div class="fr-col-6 fr-col-md-4 fr-col-lg-6">
+          <div class="fr-card fr-enlarge-link">
+              <div class="fr-card__body">
+                  <div class="fr-card__content">
+                      <h4 class="fr-card__title">
+                          <a href="https://npm3d.fr/">NPM3D</a>
+                      </h4>
+                      <p class="fr-card__desc">Jeux de nuages de points de villes françaises, pour du Lidar.</p>
+                      <!-- <div class="fr-card__end">
+                          <p class="fr-card__detail fr-icon-warning-fill">détail (optionnel)</p>
+                      </div> -->
+                  </div>
+              </div>
+              <div class="fr-card__header">
+                  <div class="fr-card__img">
+                    <img src="https://npm3d.fr/storage/pages/November2021/soufflot.jpg" class="fr-responsive-img" alt="">
+                  </div>
+              </div>
+            </div>
+  </div>
+</div>
+
+
+<br>
+<h2>Couches technologiques utilisées</h2>
+
+<h2>Autres références</h2>
+
+<ul>
+  <li></li>
+  <li></li>
+</ul>
